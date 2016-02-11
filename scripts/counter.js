1d0188f65bf1aeb29b9614c72f43e3eaf50aa47f
Max <maxpolski.job@yandex.ua>
2016-02-11 19:07:02 +0200

    control commit

--- scripts/counter.js
+++ scripts/counter.js
@@ -6,7 +6,7 @@ let counter = document.querySelector("#counter");
 let params = {
   startsFrom:      3000, //timer starting position
   endsIn:          12251, //timer end position
-  symbolsPerTick: 513, //number of symbols added per one tick
+  symbolsPerTick: 50, //number of symbols added per one tick
   counterEl:      counter, //counter element node
   tick:           45, //length of the timer tick
   mode:           "N" //type of counter animation
@@ -19,6 +19,7 @@ function startCounter(params) {
   let currentCounterValue = params.startsFrom;
   let numOfTicks  = (params.endsIn - params.startsFrom) / params.symbolsPerTick;
   let tickCounter = 0;
+  console.log('numOfTicks', numOfTicks);
   let timer = window.setInterval(() => {
     // let innerTimerTickInterval = params.tick;
     // switch(tickCounter) {
@@ -33,8 +34,14 @@ function startCounter(params) {
     if(currentCounterValue === params.endsIn) {
       clearInterval(timer);
     }
+    if(tickCounter < numOfTicks/2) {
+      params.symbolsPerTick += 11;
+      return;
+    }
+    params.symbolsPerTick = 1;
     tickCounter++;
   }, params.tick);
+  console.log('tickCounter', tickCounter);
 }
 /*
   TYPES OF COUNTER ANIMATIONS
